8357ed8d7d8cca745966e08954830475f1efbb53
TA <ta@bgn.games>
2022-11-23 16:18:03 -0500

    fixed hero and added style assets

--- pages/_app.js
+++ pages/_app.js
@@ -1,4 +1,4 @@
-import '../public/assets/css/app.2afad0c.bundle.css'
+import '../public/assets/css/style.css'
 import "../public/assets/css/swiper-custom.css";
 import React, { useEffect, useState } from "react";
 import Preloader from '../components/elements/Preloader';
