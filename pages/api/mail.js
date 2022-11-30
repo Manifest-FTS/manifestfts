b99e5c34e088129846545e7afcd83475565fa504
veselinovicdevelopment <veselinovicdevelopment@gmail.com>
2022-11-30 23:25:49 +0300

    Fixing form isue

--- pages/api/mail.js
+++ pages/api/mail.js
@@ -1,30 +1,31 @@
-// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
-const mail= require('@sendgrid/mail');
+import sendgrid from "@sendgrid/mail";
 
-mail.setApiKey(process.env.NEXT_PUBLIC_SENDGRID_API_KEY);
+sendgrid.setApiKey(process.env.NEXT_PUBLIC_SENDGRID_API_KEY);
 
-
-export default function handler(req, res) {
-  const body = JSON.parse(req.body);
+async function sendEmail(req, res) {
+  // NOTE: Uncomment the below lines to make the code work
 
   const message = `
-  Name: ${body.fullname}\r\n
-  Email: ${body.email}\r\n
-  Phone: ${body.phone}\r\n
-  Company: ${body.company}\r\n
-  Message: ${body.message}
-  `
-  ;
+  Name: ${req.body.fullname}\r\n
+  Email: ${req.body.email}\r\n
+  Phone: ${req.body.phone}\r\n
+  Company: ${req.body.company}\r\n
+  Message: ${req.body.message}
+  `;
 
-  const data = {
-    to: "manifestfts@gmail.com",
-    from: "partner@manifestfts.com",
-    subject: "New Manifest Lead",
-    text: message,
-    html: message.replace(/\r\n/g, "<br>")
-  };
+  try {
+    await sendgrid.send({
+      to: "partner@manifestfts.com",
+      from: "partner@manifestfts.com",
+      subject: "New Manifest Lead",
+      text: message,
+      html: message.replace(/\r\n/g, "<br>"),
+    });
+  } catch (error) {
+    return res.status(error.statusCode || 500).json({ error: error.message });
+  }
 
-  mail.send(data);
-
-  res.status(200).json({ status: 'Ok' })
+  res.status(200).json({ status: "Ok" });
 }
+
+export default sendEmail;
