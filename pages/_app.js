4dfeaee7effed080312a80a9007d4bc58765d426
TA <ta@bgn.games>
2022-12-01 15:11:11 -0500

    revise CRM and GTM

--- pages/_app.js
+++ pages/_app.js
@@ -6,23 +6,21 @@ import Script from 'next/script';
 
 function MyApp({ Component, pageProps }) {
     return (
-    <>
-        <Script id="FreshCRM" src='//fw-cdn.com/2297861/2939382.js' chat='true'></Script>
-        <Script id="GTM1" strategy="lazyOnload" src={`https://www.googletagmanager.com/gtm.js?id=GTM-NPL69N2`}/>
+        <>
+          <Head>
+            <title>Manifest FTS | Branding, Design and Software Development</title>
+            <meta name="description" content="We help our partners acquire new customers, get more traffic, sell more stuff and create better user experiences. Get started with a free Consultation." />
+          </Head>
+          <Script strategy="afterInteractive">
+          {`(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':` +
+            `new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],` +
+            `j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=` +
+            `'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);` +
+            `})(window,document,'script','dataLayer','GTM-NPL69N2');`}
+            </Script>
 
-        <Script id="GTM2" strategy='lazyOnload'>
-            {`
-                window.datalayer = window.dataLayer || [];
-                
-                function gtag(){dataLayer.push(arguments);}
-                
-                gtag('js', new Date());
-                gtag('config', 'GTM-NPL69N2');
-            `}
-        </Script>
-
-        <Component {...pageProps} />
-    </>
-)}
+          <Component {...pageProps} />
+        </>
+      );
 
 export default MyApp
