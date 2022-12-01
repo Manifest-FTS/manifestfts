84bd5aa953cdc50f9c79ee526c72e4731902ba56
TA <ta@bgn.games>
2022-12-01 15:16:45 -0500

    revise CRM and GTM error

--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,8 @@
 import '../public/assets/css/style.css'
 import "../public/assets/css/swiper-custom.css";
 import React, { useEffect, useState } from "react";
+import Head from 'next/head';
+import Script from 'next/script';
 import 'react-modal-video/css/modal-video.css';
 function MyApp({ Component, pageProps }) {
     return (
