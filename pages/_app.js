b0ffd5d224b49c125924208676a78b95a799ec95
TA <ta@bgn.games>
2022-12-01 15:14:52 -0500

    revise CRM and GTM error

--- pages/_app.js
+++ pages/_app.js
@@ -2,25 +2,24 @@ import '../public/assets/css/style.css'
 import "../public/assets/css/swiper-custom.css";
 import React, { useEffect, useState } from "react";
 import 'react-modal-video/css/modal-video.css';
-import Script from 'next/script';
-
 function MyApp({ Component, pageProps }) {
     return (
         <>
-          <Head>
+            <Head>
             <title>Manifest FTS | Branding, Design and Software Development</title>
             <meta name="description" content="We help our partners acquire new customers, get more traffic, sell more stuff and create better user experiences. Get started with a free Consultation." />
-          </Head>
-          <Script id="GTM" strategy="afterInteractive">
-          {`(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':` +
+            </Head>
+            <Script id="GTM" strategy="afterInteractive">
+            {`(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':` +
             `new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],` +
             `j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=` +
             `'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);` +
             `})(window,document,'script','dataLayer','GTM-NPL69N2');`}
             </Script>
 
-          <Component {...pageProps} />
+            <Component {...pageProps} />
         </>
-      );
+    )
+}
 
 export default MyApp
