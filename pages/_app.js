6affa277e187158c3c1b893d25b8b44299f4e006
TA <ta@bgn.games>
2022-12-01 14:21:50 -0500

    added GTM

--- pages/_app.js
+++ pages/_app.js
@@ -2,8 +2,26 @@ import '../public/assets/css/style.css'
 import "../public/assets/css/swiper-custom.css";
 import React, { useEffect, useState } from "react";
 import 'react-modal-video/css/modal-video.css';
+import Script from 'next/script';
+
 function MyApp({ Component, pageProps }) {
-    return <Component {...pageProps} />
-}
+    return (
+    <>
+        <Script strategy="lazyOnload" src={`https://www.googletagmanager.com/gtag/js?id=GTM-NPL69N2`}/>
+
+        <Script strategy='lazyOnload'>
+            {`
+                window.datalayer = window.dataLayer || [];
+                
+                function gtag(){dataLayer.push(arguments);}
+                
+                gtag('js', new Date());
+                gtag('config', 'GTM-NPL69N2');
+            `}
+        </Script>
+
+        <Component {...pageProps} />
+    </>
+)}
 
 export default MyApp
