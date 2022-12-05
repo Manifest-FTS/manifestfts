cd9d392d501952541d20be9d8f01497b87c17336
veselinovicdevelopment <veselinovicdevelopment@gmail.com>
2022-12-05 12:52:11 +0300

    adding document

--- pages/_app.js
+++ pages/_app.js
@@ -3,27 +3,33 @@ import "../public/assets/css/swiper-custom.css";
 import "../styles/globals.scss";
 
 import React, { useEffect, useState } from "react";
-import Head from 'next/head';
-import Script from 'next/script';
-import 'react-modal-video/css/modal-video.css';
+import Head from "next/head";
+import Script from "next/script";
+import "react-modal-video/css/modal-video.css";
 function MyApp({ Component, pageProps }) {
-    return (
-        <>
-            <Head>
-            <title>Manifest FTS | Branding, Design and Software Development</title>
-            <meta name="description" content="We help our partners acquire new customers, get more traffic, sell more stuff and create better user experiences. Get started with a free Consultation." />
-            </Head>
-            <Script id="GTM" strategy="afterInteractive">
-            {`(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':` +
-            `new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],` +
-            `j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=` +
-            `'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);` +
-            `})(window,document,'script','dataLayer','GTM-NPL69N2');`}
-            </Script>
+  return (
+    <>
+      <Head>
+        <title>Manifest FTS | Branding, Design and Software Development</title>
+        <meta
+          name="description"
+          content="We help our partners acquire new customers, get more traffic, sell more stuff and create better user experiences. Get started with a free Consultation."
+        />
+      </Head>
 
-            <Component {...pageProps} />
-        </>
-    )
+      <Script id="GTM" strategy="afterInteractive">
+        {`
+        (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+        new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+        j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+        'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+        })(window,document,'script','dataLayer','GTM-NPL69N2');
+      `}
+      </Script>
+
+      <Component {...pageProps} />
+    </>
+  );
 }
 
 export default MyApp;
---
+++ pages/_document.js
@@ -0,0 +1,31 @@
+import Document, { Html, Head, Main, NextScript } from "next/document";
+
+class MyDocument extends Document {
+  render() {
+    return (
+      <Html lang="en">
+        <Head>
+          <link rel="apple-touch-icon" href="/icon.png" />
+          <meta
+            content="text/html; charset=UTF-8; X-Content-Type-Options=nosniff"
+            httpEquiv="Content-Type"
+          />
+
+          <meta name="theme-color" content="#7000FF" />
+        </Head>
+        <body>
+          <Main />
+          <NextScript />
+          <noscript
+            dangerouslySetInnerHTML={{
+              __html: `<iframe src="https://www.googletagmanager.com/ns.html?id=GTM-NPL69N2"
+              height="0" width="0" style="display:none;visibility:hidden"></iframe>`,
+            }}
+          />
+        </body>
+      </Html>
+    );
+  }
+}
+
+export default MyDocument;
