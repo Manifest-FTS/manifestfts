fdd8db982aefd29ab657f62a7e2bb703f5c4b649
TA <ta@bgn.games>
2022-12-08 14:04:53 -0500

    added meta title and description

--- pages/_document.js
+++ pages/_document.js
@@ -4,19 +4,22 @@ import { GTM_ID, pageview } from '../lib/gtm'
 export default function Document() {
     return (
         <Html>
-        <Head/>
-        <body>
-            <noscript>
-            <iframe
-                src={`https://www.googletagmanager.com/ns.html?id=${GTM_ID}`}
-                height="0"
-                width="0"
-                style={{ display: 'none', visibility: 'hidden' }}
-            />
-            </noscript>
-            <Main />
-            <NextScript />
-        </body>
+            <Head>
+                <title>Manifest FTS | Branding, Design Strategy and Software Solutions</title>
+                <meta name="description" content="We help our partners acquire new customers, increase donations and donor activity, sell more stuff and create better user experiences."/>
+            </Head>
+            <body>
+                <noscript>
+                <iframe
+                    src={`https://www.googletagmanager.com/ns.html?id=${GTM_ID}`}
+                    height="0"
+                    width="0"
+                    style={{ display: 'none', visibility: 'hidden' }}
+                />
+                </noscript>
+                <Main />
+                <NextScript />
+            </body>
         </Html>
     )
 }
