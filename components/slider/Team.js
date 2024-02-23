ab915c7c1f8b99beb4c97c060e86547499e4af0f
Md Masum <masum@talentedcube.com>
2024-02-23 14:25:31 -0600

    Team section added in about page, and a little change in the page cover

--- components/slider/Team.js
+++ components/slider/Team.js
@@ -37,141 +37,61 @@ const TeamSlider = () => {
 
     return (
         <>
-            <div className="box-swiper">
-                <div className="swiper-container swiper-group-1">
-                    <Swiper
-                        slidesPerView={1}
-                        spaceBetween={30}
-                        loop={true}
-                        autoplay={{
-                            delay: 2500,
-                            disableOnInteraction: false
-                        }}
-                        navigation={{
-                            prevEl: ".swiper-button-prev-4",
-                            nextEl: ".swiper-button-next-4",
-                        }}
-                        className="swiper-wrapper pb-70 pt-5"
-                    >
-                        {data.map((item, i) => (
-                            <SwiperSlide>
-                                <div className="swiper-slide active">
-                                    <div className="row">
-                                        <div className="col-lg-3 col-md-6 col-sm-6">
-                                            <div className="card-grid-style-5 hover-up">
-                                                <div className="grid-5-img mb-15"><img src="/assets/imgs/page/about/1/team-1.png" alt="Agon" /></div><span className="text-body-text-md color-gray-500">Marketing CEO</span>
-                                                <h3 className="text-heading-5 mb-5 mt-5">Theresa Webb</h3>
-                                                <p className="text-body-excerpt text-desc color-gray-500 mt-15 mb-20">Lorem ipsum dolor sit amet consectetur imp adipiscing elit justo</p><span className="text-body-text-md color-gray-600">Let’s Connect</span>
-                                                <div className="social-bottom">
-                                                    <Link href="#"><a className="icon-socials icon-facebook"></a></Link>
-                                                    <Link href="#"><a className="icon-socials icon-twitter"></a></Link>
-                                                    <Link href="#"><a className="icon-socials icon-instagram"></a></Link>
-                                                    <Link href="#"><a className="icon-socials icon-linkedin"></a></Link>
-                                                </div>
-                                            </div>
-                                        </div>
-                                        <div className="col-lg-3 col-md-6 col-sm-6">
-                                            <div className="card-grid-style-5 hover-up">
-                                                <div className="grid-5-img mb-15"><img src="/assets/imgs/page/about/1/team-2.png" alt="Agon" /></div><span className="text-body-text-md color-gray-500">Marketing CEO</span>
-                                                <h3 className="text-heading-5 mb-5 mt-5">Theresa Webb</h3>
-                                                <p className="text-body-excerpt text-desc color-gray-500 mt-15 mb-20">Lorem ipsum dolor sit amet consectetur imp adipiscing elit justo</p><span className="text-body-text-md color-gray-600">Let’s Connect</span>
-                                                <div className="social-bottom">
-                                                    <Link href="#"><a className="icon-socials icon-facebook"></a></Link>
-                                                    <Link href="#"><a className="icon-socials icon-twitter"></a></Link>
-                                                    <Link href="#"><a className="icon-socials icon-instagram"></a></Link>
-                                                    <Link href="#"><a className="icon-socials icon-linkedin"></a></Link>
-                                                </div>
-                                            </div>
-                                        </div>
-                                        <div className="col-lg-3 col-md-6 col-sm-6">
-                                            <div className="card-grid-style-5 hover-up">
-                                                <div className="grid-5-img mb-15"><img src="/assets/imgs/page/about/1/team-3.png" alt="Agon" /></div><span className="text-body-text-md color-gray-500">Marketing CEO</span>
-                                                <h3 className="text-heading-5 mb-5 mt-5">Theresa Webb</h3>
-                                                <p className="text-body-excerpt text-desc color-gray-500 mt-15 mb-20">Lorem ipsum dolor sit amet consectetur imp adipiscing elit justo</p><span className="text-body-text-md color-gray-600">Let’s Connect</span>
-                                                <div className="social-bottom">
-                                                    <Link href="#"><a className="icon-socials icon-facebook"></a></Link>
-                                                    <Link href="#"><a className="icon-socials icon-twitter"></a></Link>
-                                                    <Link href="#"><a className="icon-socials icon-instagram"></a></Link>
-                                                    <Link href="#"><a className="icon-socials icon-linkedin"></a></Link>
-                                                </div>
-                                            </div>
-                                        </div>
-                                        <div className="col-lg-3 col-md-6 col-sm-6">
-                                            <div className="card-grid-style-5 hover-up">
-                                                <div className="grid-5-img mb-15"><img src="/assets/imgs/page/about/1/team-4.png" alt="Agon" /></div><span className="text-body-text-md color-gray-500">Marketing CEO</span>
-                                                <h3 className="text-heading-5 mb-5 mt-5">Theresa Webb</h3>
-                                                <p className="text-body-excerpt text-desc color-gray-500 mt-15 mb-20">Lorem ipsum dolor sit amet consectetur imp adipiscing elit justo</p><span className="text-body-text-md color-gray-600">Let’s Connect</span>
-                                                <div className="social-bottom">
-                                                    <Link href="#"><a className="icon-socials icon-facebook"></a></Link>
-                                                    <Link href="#"><a className="icon-socials icon-twitter"></a></Link>
-                                                    <Link href="#"><a className="icon-socials icon-instagram"></a></Link>
-                                                    <Link href="#"><a className="icon-socials icon-linkedin"></a></Link>
-                                                </div>
-                                            </div>
-                                        </div>
-                                        <div className="col-lg-3 col-md-6 col-sm-6">
-                                            <div className="card-grid-style-5 hover-up">
-                                                <div className="grid-5-img mb-15"><img src="/assets/imgs/page/about/1/team-5.png" alt="Agon" /></div><span className="text-body-text-md color-gray-500">Marketing CEO</span>
-                                                <h3 className="text-heading-5 mb-5 mt-5">Theresa Webb</h3>
-                                                <p className="text-body-excerpt text-desc color-gray-500 mt-15 mb-20">Lorem ipsum dolor sit amet consectetur imp adipiscing elit justo</p><span className="text-body-text-md color-gray-600">Let’s Connect</span>
-                                                <div className="social-bottom">
-                                                    <Link href="#"><a className="icon-socials icon-facebook"></a></Link>
-                                                    <Link href="#"><a className="icon-socials icon-twitter"></a></Link>
-                                                    <Link href="#"><a className="icon-socials icon-instagram"></a></Link>
-                                                    <Link href="#"><a className="icon-socials icon-linkedin"></a></Link>
-                                                </div>
-                                            </div>
-                                        </div>
-                                        <div className="col-lg-3 col-md-6 col-sm-6">
-                                            <div className="card-grid-style-5 hover-up">
-                                                <div className="grid-5-img mb-15"><img src="/assets/imgs/page/about/1/team-6.png" alt="Agon" /></div><span className="text-body-text-md color-gray-500">Marketing CEO</span>
-                                                <h3 className="text-heading-5 mb-5 mt-5">Theresa Webb</h3>
-                                                <p className="text-body-excerpt text-desc color-gray-500 mt-15 mb-20">Lorem ipsum dolor sit amet consectetur imp adipiscing elit justo</p><span className="text-body-text-md color-gray-600">Let’s Connect</span>
-                                                <div className="social-bottom">
-                                                    <Link href="#"><a className="icon-socials icon-facebook"></a></Link>
-                                                    <Link href="#"><a className="icon-socials icon-twitter"></a></Link>
-                                                    <Link href="#"><a className="icon-socials icon-instagram"></a></Link>
-                                                    <Link href="#"><a className="icon-socials icon-linkedin"></a></Link>
-                                                </div>
-                                            </div>
-                                        </div>
-                                        <div className="col-lg-3 col-md-6 col-sm-6">
-                                            <div className="card-grid-style-5 hover-up">
-                                                <div className="grid-5-img mb-15"><img src="/assets/imgs/page/about/1/team-7.png" alt="Agon" /></div><span className="text-body-text-md color-gray-500">Marketing CEO</span>
-                                                <h3 className="text-heading-5 mb-5 mt-5">Theresa Webb</h3>
-                                                <p className="text-body-excerpt text-desc color-gray-500 mt-15 mb-20">Lorem ipsum dolor sit amet consectetur imp adipiscing elit justo</p><span className="text-body-text-md color-gray-600">Let’s Connect</span>
-                                                <div className="social-bottom">
-                                                    <Link href="#"><a className="icon-socials icon-facebook"></a></Link>
-                                                    <Link href="#"><a className="icon-socials icon-twitter"></a></Link>
-                                                    <Link href="#"><a className="icon-socials icon-instagram"></a></Link>
-                                                    <Link href="#"><a className="icon-socials icon-linkedin"></a></Link>
-                                                </div>
-                                            </div>
-                                        </div>
-                                        <div className="col-lg-3 col-md-6 col-sm-6">
-                                            <div className="card-grid-style-5 hover-up">
-                                                <div className="grid-5-img mb-15"><img src="/assets/imgs/page/about/1/team-8.png" alt="Agon" /></div><span className="text-body-text-md color-gray-500">Marketing CEO</span>
-                                                <h3 className="text-heading-5 mb-5 mt-5">Theresa Webb</h3>
-                                                <p className="text-body-excerpt text-desc color-gray-500 mt-15 mb-20">Lorem ipsum dolor sit amet consectetur imp adipiscing elit justo</p><span className="text-body-text-md color-gray-600">Let’s Connect</span>
-                                                <div className="social-bottom">
-                                                    <Link href="#"><a className="icon-socials icon-facebook"></a></Link>
-                                                    <Link href="#"><a className="icon-socials icon-twitter"></a></Link>
-                                                    <Link href="#"><a className="icon-socials icon-instagram"></a></Link>
-                                                    <Link href="#"><a className="icon-socials icon-linkedin"></a></Link>
-                                                </div>
-                                            </div>
-                                        </div>
-                                    </div>
-                                </div>
-                            </SwiperSlide>
-                        ))}
-                    </Swiper>
+            <div className="row">
+                <div className="col-lg-3 col-md-6 col-sm-6">
+                    <div className="card-grid-style-5 hover-up">
+                        <div className="grid-5-img mb-15"><img src="/assets/imgs/page/about/1/kevin_williams.png" alt="Agon"/>
+                        </div>
+                        <span className="text-body-text-md color-gray-500">Founder, Chief Technology Officer</span>
+                        <h3 className="text-heading-5 mb-5 mt-5">Kevin Williams</h3>
+                        <span className="text-body-text-md color-gray-600">Let’s Connect</span>
+                        <div className="social-bottom">
+                            <Link href="mailto:devkev@manifestfts.com"><a
+                                className="icon-socials icon-email"></a></Link>
+                            <Link href="http://instagram.com/kevinorin"><a className="icon-socials icon-instagram"></a></Link>
+                            <Link href="http://linkedin.com/in/kevinorin"><a className="icon-socials icon-linkedin"></a></Link>
+                        </div>
+                    </div>
                 </div>
-                <div className="swiper-button-next swiper-button-next-4" />
-                <div className="swiper-button-prev swiper-button-prev-4" />
+                <div className="col-lg-3 col-md-6 col-sm-6">
+                    <div className="card-grid-style-5 hover-up">
+                        <div className="grid-5-img mb-15"><img src="/assets/imgs/page/about/1/mohammed_masum.png" alt="Agon"/>
+                        </div>
+                        <span className="text-body-text-md color-gray-500">Product & Project Manager</span>
+                        <h3 className="text-heading-5 mb-5 mt-5">Mohammed Masum</h3>
+                        <span className="text-body-text-md color-gray-600">Let’s Connect</span>
+                        <div className="social-bottom">
+                            <Link href="mailto:mdm@manifestfts.com"><a className="icon-socials icon-email"></a></Link>
+                            <Link href="https://www.linkedin.com/in/mmasum/"><a
+                                className="icon-socials icon-linkedin"></a></Link>
+                        </div>
+                    </div>
+                </div>
+                <div className="col-lg-3 col-md-6 col-sm-6">
+                    <div className="card-grid-style-5 hover-up">
+                        <div className="grid-5-img mb-15"><img src="/assets/imgs/page/about/1/profile.jpg" alt="Agon"/>
+                        </div>
+                        <span className="text-body-text-md color-gray-500">Client Relations Manager</span>
+                        <h3 className="text-heading-5 mb-5 mt-5">Daniel Kane</h3>
+                        <span className="text-body-text-md color-gray-600">Let’s Connect</span>
+                        <div className="social-bottom">
+                            <Link href="mailto:hello@manifestfts.com"><a className="icon-socials icon-email"></a></Link>
+                        </div>
+                    </div>
+                </div>
+                <div className="col-lg-3 col-md-6 col-sm-6">
+                    <div className="card-grid-style-5 hover-up">
+                        <div className="grid-5-img mb-15"><img src="/assets/imgs/page/about/1/profile.jpg" alt="Agon"/>
+                        </div>
+                        <span className="text-body-text-md color-gray-500">Software Engineer, Solutions Architect</span>
+                        <h3 className="text-heading-5 mb-5 mt-5">Arun Jha</h3>
+                        <span className="text-body-text-md color-gray-600">Let’s Connect</span>
+                        <div className="social-bottom">
+                            <Link href="mailto:arunj@manifestfts.com"><a className="icon-socials icon-email"></a></Link>
+                        </div>
+                    </div>
+                </div>
             </div>
-
-
         </>
     );
 };
